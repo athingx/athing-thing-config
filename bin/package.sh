a20dfb72c09dde506cdde97185e238e7754b5708
vlinux <oldmanpushcart@gmail.com>
2021-10-03 01:12:28 +0800

    去掉Boot加载方式

--- bin/package.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-projects[i++]="com.github.athingx.athing.aliyun.config:config-boot"
-
-# maven package boot projects
-mvn clean package \
-  -f ../pom.xml \
-  -pl "$(printf "%s," "${projects[@]}")" -am \
-  '-Dmaven.test.skip=true'
