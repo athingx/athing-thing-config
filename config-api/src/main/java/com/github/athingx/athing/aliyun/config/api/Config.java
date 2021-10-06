7ce8a25ed14de42c25c265dd2ed78d92244ea705
vlinux <oldmanpushcart@gmail.com>
2021-10-06 13:18:14 +0800

    Revert "合并api和component包"
    This reverts commit 7e565dd0d821daeb8027052abee26322879addf6.

---
+++ config-api/src/main/java/com/github/athingx/athing/aliyun/config/api/Config.java
@@ -0,0 +1,31 @@
+package com.github.athingx.athing.aliyun.config.api;
+
+import com.github.athingx.athing.standard.thing.op.executor.ThingFuture;
+
+/**
+ * 设备配置
+ */
+public interface Config {
+
+    /**
+     * 获取配置版本号
+     *
+     * @return 配置版本号
+     */
+    String getVersion();
+
+    /**
+     * 获取配置范围
+     *
+     * @return 配置范围
+     */
+    Scope getScope();
+
+    /**
+     * 获取配置内容
+     *
+     * @return 获取凭证
+     */
+    ThingFuture<String> getContent();
+
+}
